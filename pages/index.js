2eb642249ee15b6f1ec99cc3af9c3d9e00c63933
Jose Sandoval <joseasandovalp@gmail.com>
2021-12-02 17:57:31 +0100

    ✨ remove unnecesary boilerplate from index page

--- pages/index.js
+++ pages/index.js
@@ -1,32 +1,12 @@
-import {
-  Button,
-  Flex,
-  Heading,
-  Input,
-  useColorMode,
-  useColorModeValue,
-} from "@chakra-ui/react";
+import Head from "next/head";
 
 export default function HomePage() {
-  const { toggleColorMode } = useColorMode();
-  const formBackground = useColorModeValue("gray.100", "gray.700");
-
   return (
-    <Flex height="100vh" alignItems="center" justifyContent="center">
-      <Flex direction="column" background={formBackground} p={12} rounded={6}>
-        <Heading mb={6}>Log in</Heading>
-        <Input
-          placeholder="email@epic.com"
-          variant="filled"
-          mb={3}
-          type="email"
-        />
-        <Input placeholder="******" variant="filled" mb={6} type="password" />
-        <Button mb={6} colorScheme="teal">
-          Log in
-        </Button>
-        <Button onClick={toggleColorMode}>Toggle theme</Button>
-      </Flex>
-    </Flex>
+    <>
+      <Head>
+        <title>Dashboard | NFTs Market</title>
+      </Head>
+      <h1>HomePage</h1>
+    </>
   );
 }
