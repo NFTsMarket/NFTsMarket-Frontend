5622e798e98c31a58e9e613d1ca3a039eff0a6d4
reyblacua <reyblacua@alum.us.es>
2021-12-12 17:27:00 +0100

    feat: Añadida vista de listado de assets
    Actualmente tiene datos estáticos. Issue #14

---
+++ pages/upload/asset.jsx
@@ -0,0 +1,35 @@
+import {
+    Box,
+    Center,
+    useColorModeValue,
+    Heading,
+    Text,
+    Stack,
+    Divider,
+    Image,
+    Button,
+  } from "@chakra-ui/react";
+  import { DeleteIcon, EditIcon } from "@chakra-ui/icons";
+  
+  export default function Asset(props) {
+    const ID = props.asset.id,
+      URL = props.asset.url;
+  
+    return (
+      <Center py={12}>
+            <Image
+              rounded={"lg"}
+              height={390}
+              width={300}
+              objectFit={"cover"}
+              src={URL}
+              alt={""}
+              _hover={{
+                transform: "scale(1.03)",
+              }}
+              boxShadow={"2xl"}
+              rounded={"lg"}
+            />
+      </Center>
+    );
+  }
