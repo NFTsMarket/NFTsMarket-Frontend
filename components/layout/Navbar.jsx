73fad8aee956d3d916ae8a5c906b53cb3c67a98a
Jose Sandoval <joseasandovalp@gmail.com>
2021-12-17 19:00:05 +0100

    feat: add links to sign up and log in buttons

--- components/layout/Navbar.jsx
+++ components/layout/Navbar.jsx
@@ -5,11 +5,11 @@ import {
   Stack,
   Heading,
   Flex,
-  Text,
   Button,
   useDisclosure,
   useColorMode,
   useColorModeValue,
+  Link as ChakraLink,
 } from "@chakra-ui/react";
 import { HamburgerIcon, MoonIcon, SunIcon } from "@chakra-ui/icons";
 
@@ -53,19 +53,19 @@ export default function Navbar(props) {
         spacing="32px"
       >
         <Link href="/" passHref>
-          <Text cursor="pointer">Catalog</Text>
+          <ChakraLink>Catalog</ChakraLink>
         </Link>
         <Link href="/assets" passHref>
-          <Text cursor="pointer">My Assets</Text>
+          <ChakraLink>My Assets</ChakraLink>
         </Link>
         <Link href="/sales" passHref>
-          <Text cursor="pointer">Pending Sales</Text>
+          <ChakraLink>Pending Sales</ChakraLink>
         </Link>
         <Link href="/purchases" passHref>
-          <Text cursor="pointer">Purchases</Text>
+          <ChakraLink>Purchases</ChakraLink>
         </Link>
         <Link href="/wallet" passHref>
-          <Text cursor="pointer">Wallet</Text>
+          <ChakraLink>Wallet</ChakraLink>
         </Link>
       </Stack>
 
@@ -80,14 +80,18 @@ export default function Navbar(props) {
         >
           {colorMode === "light" ? <MoonIcon /> : <SunIcon />}
         </Button>
-        <Button
-          variant="outline"
-          _hover={{ color: "purple.500", bg: "white" }}
-          mr={4}
-        >
-          Sign Up
-        </Button>
-        <Button colorScheme="purple">Log In</Button>
+        <Link href="/signup" passHref>
+          <Button
+            variant="outline"
+            _hover={{ color: "purple.500", bg: "white" }}
+            mr={4}
+          >
+            Sign Up
+          </Button>
+        </Link>
+        <Link href="/login" passHref>
+          <Button colorScheme="purple">Log In</Button>
+        </Link>
       </Box>
     </Flex>
   );
