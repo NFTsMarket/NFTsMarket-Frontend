28c65b9dda1d8e49cb2a0d3f13411a0ad882feaf
Jose Sandoval <joseasandovalp@gmail.com>
2021-12-19 23:58:30 +0100

    feat: add graphql client

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,22 @@
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
 import "../styles/globals.css";
 
+const graphQLClient = new ApolloClient({
+  uri: "https://api-gateway-bujosa.cloud.okteto.net/graphql",
+  cache: new InMemoryCache(),
+});
+
 function MyApp({ Component, pageProps }) {
   return (
-    <ChakraProvider>
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ChakraProvider>
+    <ApolloProvider client={graphQLClient}>
+      <ChakraProvider>
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
