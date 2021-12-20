cf4ad4746767e0d2c5825cc023b0766f9fb3ca21
Jose Sandoval <joseasandovalp@gmail.com>
2021-12-20 12:31:31 +0100

    feat: add auth context

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,44 @@
-import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+import {
+  ApolloClient,
+  ApolloProvider,
+  createHttpLink,
+  InMemoryCache,
+} from "@apollo/client";
+import { setContext } from "@apollo/client/link/context";
 import { ChakraProvider } from "@chakra-ui/react";
 import Layout from "../components/layout/Layout";
+import { AuthProvider } from "../context/AuthContext";
 import "../styles/globals.css";
 
-const graphQLClient = new ApolloClient({
+const httpLink = createHttpLink({
   uri: "https://api-gateway-bujosa.cloud.okteto.net/graphql",
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem("token");
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
+const graphQLClient = new ApolloClient({
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
 function MyApp({ Component, pageProps }) {
   return (
     <ApolloProvider client={graphQLClient}>
-      <ChakraProvider>
-        <Layout>
-          <Component {...pageProps} />
-        </Layout>
-      </ChakraProvider>
+      <AuthProvider>
+        <ChakraProvider>
+          <Layout>
+            <Component {...pageProps} />
+          </Layout>
+        </ChakraProvider>
+      </AuthProvider>
     </ApolloProvider>
   );
 }
