9c724f01a2c929dd4f18b0ede9545814ed82bdf8
Jose Sandoval <joseasandovalp@gmail.com>
2021-12-22 20:29:33 +0100

    fix: reduce viewport height

--- components/common/CenteredText.jsx
+++ components/common/CenteredText.jsx
@@ -2,7 +2,7 @@ import { Center, Text } from "@chakra-ui/react";
 
 function CenteredText({ children }) {
   return (
-    <Center h="80vh" w="100%">
+    <Center h="75vh" w="100%">
       <Text fontSize="xl">{children}</Text>
     </Center>
   );
