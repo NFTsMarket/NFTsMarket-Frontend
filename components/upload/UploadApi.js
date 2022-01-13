a608657ea239465cd17bc0d157ef1a2f9368c62f
reyblacua <reyblacua@alum.us.es>
2022-01-13 19:38:37 +0100

    feat: Integrado la vista de un asset
    Se ha integrado el get individual de un asset en front. Issue #20

--- components/upload/UploadApi.js
+++ components/upload/UploadApi.js
@@ -1,10 +1,8 @@
-import { useAuth } from "../../context/AuthContext";
-
-
 class UploadApi {
 
-    static API_BASE_URL="https://api-juaferfer11.cloud.okteto.net";
-    
+    // static API_BASE_URL="https://api-juaferfer11.cloud.okteto.net/api/v1/asset";
+    static API_BASE_URL="http://localhost:8000/api/v1/asset";
+
     static requestHeaders(){
         return {
             "Authorization":"Bearer "+localStorage.getItem("token")
@@ -15,6 +13,23 @@ class UploadApi {
         const headers= this.requestHeaders();
         const user=JSON.parse(localStorage.getItem("user"));
         const request= new Request(this.API_BASE_URL+"?user="+user.id,{
+            method:"GET",
+            headers:headers
+        })
+
+        const response= await fetch(request);
+
+        if(!response.ok){
+            throw Error("Response not valid:"+ response.status);
+        }
+
+        return response.json();
+    }
+
+    static async getAsset(id){
+        const headers= this.requestHeaders();
+        const user=JSON.parse(localStorage.getItem("user"));
+        const request= new Request(this.API_BASE_URL+"/"+id,{
             method:"GET",
             headers:headers
         })
