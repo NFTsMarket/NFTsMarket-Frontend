d7911766636ca130d7ceea8843da9b71510dc1ff
Jose Sandoval <joseasandovalp@gmail.com>
2022-01-15 19:11:55 +0100

    chore: remove facebook auth

--- components/auth/SocialButtons.js
+++ components/auth/SocialButtons.js
@@ -1,13 +1,9 @@
 import { Button, SimpleGrid, VisuallyHidden } from "@chakra-ui/react";
-import { FaFacebook, FaGoogle } from "react-icons/fa";
+import { FaGoogle } from "react-icons/fa";
 
 function SocialButtons() {
   return (
-    <SimpleGrid mt="6" columns={2} spacing="3">
-      <Button color="currentColor" variant="outline">
-        <VisuallyHidden>Sign up with Facebook</VisuallyHidden>
-        <FaFacebook />
-      </Button>
+    <SimpleGrid mt="6" columns={1} spacing="3">
       <Button color="currentColor" variant="outline">
         <VisuallyHidden>Sign up with Google</VisuallyHidden>
         <FaGoogle />
