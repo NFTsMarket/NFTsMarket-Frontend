8b0f41e73c66d1d7351164a7ec06ca53526eece5
Jose Sandoval <joseasandovalp@gmail.com>
2022-01-15 17:28:36 +0100

    feat: convert loading spinner to modal

--- components/common/LoadingSpinner.jsx
+++ components/common/LoadingSpinner.jsx
@@ -1,10 +1,36 @@
-import { Center, Spinner } from "@chakra-ui/react";
+import { useEffect } from "react";
 
-function LoadingSpinner() {
+import {
+  Flex,
+  Modal,
+  ModalContent,
+  ModalOverlay,
+  Spinner,
+  useDisclosure,
+} from "@chakra-ui/react";
+
+function LoadingSpinner({ loading }) {
+  const { isOpen, onClose } = useDisclosure({ defaultIsOpen: true });
+
+  useEffect(() => {
+    if (!loading) onClose();
+  }, [loading]);
+
   return (
-    <Center h="100vh" w="100%">
-      <Spinner size="xl" colorScheme="purple" />
-    </Center>
+    <Modal
+      isOpen={isOpen}
+      onClose={onClose}
+      isCentered
+      closeOnOverlayClick={false}
+      closeOnEsc={false}
+    >
+      <ModalOverlay />
+      <ModalContent maxW="120">
+        <Flex justifyContent="center" alignItems="center">
+          <Spinner margin="30" size="xl" colorScheme="purple" />
+        </Flex>
+      </ModalContent>
+    </Modal>
   );
 }
 
