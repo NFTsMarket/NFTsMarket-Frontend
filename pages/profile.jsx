ce9fc68ad4c1444aaa3f921caff32c57b9b4ce3c
Jose Sandoval <joseasandovalp@gmail.com>
2022-01-16 18:28:39 +0100

    feat: improve profile page

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,18 +1,64 @@
+import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
+import Head from "next/head";
+import AuthCard from "../components/auth/AuthCard";
+import AuthText from "../components/auth/AuthText";
+import OldPasswordForm from "../components/auth/forms/OldPasswordForm";
 import { useAuth } from "../context/AuthContext";
 
 export default function Profile() {
   const { user } = useAuth();
 
   return (
-    <div>
-      {user && (
-        <div>
-          <h1>Welcome to your profile</h1>
-          <p>{user.email}</p>
-          <p>{user.name}</p>
-          <img src={user.profilePicture} alt={user.name} />
-        </div>
-      )}
-    </div>
+    <>
+      <Head>
+        <title>Profile | NFTs Market</title>
+      </Head>
+
+      <Box py="12">
+        <AuthText headingText="Welcome to your profile" />
+        <Flex
+          justifyContent="center"
+          alignItems="center"
+          mt="10"
+          gridGap="10"
+          direction={{ base: "column", md: "row" }}
+        >
+          <AuthCard>
+            <AuthText
+              headingText="User Info"
+              size="lg"
+              fontWeight="semibold"
+              mb="7"
+            />
+            {user && (
+              <VStack spacing={6} align="flex-start">
+                <Avatar
+                  alignSelf="center"
+                  size="2xl"
+                  name={user.name}
+                  src={user.profilePicture}
+                />
+                <Text fontSize="md">
+                  <b>email:</b> {user.email}
+                </Text>
+                <Text fontSize="md">
+                  <b>name:</b> {user.name}
+                </Text>
+              </VStack>
+            )}
+          </AuthCard>
+
+          <AuthCard>
+            <AuthText
+              headingText="Change password"
+              size="lg"
+              fontWeight="semibold"
+              mb="7"
+            />
+            <OldPasswordForm />
+          </AuthCard>
+        </Flex>
+      </Box>
+    </>
   );
 }
