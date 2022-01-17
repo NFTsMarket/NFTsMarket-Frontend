38094d01259845d84ac3fb733f3e4f7c47b72bb3
Jose Sandoval <joseasandovalp@gmail.com>
2022-01-17 10:06:06 +0100

    feat: add profile picture functionality

--- pages/profile.jsx
+++ pages/profile.jsx
@@ -1,12 +1,73 @@
-import { Avatar, Box, Flex, Text, VStack } from "@chakra-ui/react";
+import { useLazyQuery } from "@apollo/client";
+import {
+  Avatar,
+  Box,
+  Button,
+  Flex,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+  Text,
+  useDisclosure,
+  VStack,
+} from "@chakra-ui/react";
 import Head from "next/head";
 import AuthCard from "../components/auth/AuthCard";
 import AuthText from "../components/auth/AuthText";
 import OldPasswordForm from "../components/auth/forms/OldPasswordForm";
+import LoadingSpinner from "../components/common/LoadingSpinner";
 import { useAuth } from "../context/AuthContext";
+import { GET_USER } from "../utils/gqlMutations";
 
 export default function Profile() {
-  const { user } = useAuth();
+  const { user, token, dispatch } = useAuth();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [getPhotoURL, { loading }] = useLazyQuery(GET_USER, {
+    fetchPolicy: "network-only",
+  });
+
+  const updatePhoto = async ({
+    target: {
+      validity,
+      files: [file],
+    },
+  }) => {
+    if (validity.valid) {
+      const formData = new FormData();
+      formData.append("file", file);
+
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_API_GATEWAY_DOMAIN}/profilePicture`,
+        {
+          method: "POST",
+          headers: {
+            Authorization: token ? `Bearer ${token}` : "",
+          },
+          body: formData,
+        }
+      );
+      const profileChanged = await response.json();
+
+      if (profileChanged) {
+        const {
+          data: {
+            self: { profilePicture },
+          },
+        } = await getPhotoURL();
+
+        dispatch({
+          type: "UPDATE_PICTURE",
+          payload: {
+            profilePicture: profilePicture,
+          },
+        });
+      }
+    }
+  };
 
   return (
     <>
@@ -14,11 +75,13 @@ export default function Profile() {
         <title>Profile | NFTs Market</title>
       </Head>
 
+      {loading && <LoadingSpinner loading={loading} />}
+
       <Box py="12">
         <AuthText headingText="Welcome to your profile" />
         <Flex
           justifyContent="center"
-          alignItems="center"
+          alignItems={{ base: "center", md: "unset" }}
           mt="10"
           gridGap="10"
           direction={{ base: "column", md: "row" }}
@@ -32,12 +95,22 @@ export default function Profile() {
             />
             {user && (
               <VStack spacing={6} align="flex-start">
-                <Avatar
-                  alignSelf="center"
-                  size="2xl"
-                  name={user.name}
-                  src={user.profilePicture}
-                />
+                <VStack alignSelf="center" spacing={4}>
+                  <Avatar
+                    size="2xl"
+                    name={user.name}
+                    src={user.profilePicture}
+                  />
+                  <Button
+                    size="md"
+                    type="file"
+                    variant="ghost"
+                    colorScheme="purple"
+                    onClick={onOpen}
+                  >
+                    replace photo
+                  </Button>
+                </VStack>
                 <Text fontSize="md">
                   <b>email:</b> {user.email}
                 </Text>
@@ -59,6 +132,23 @@ export default function Profile() {
           </AuthCard>
         </Flex>
       </Box>
+
+      <Modal isOpen={isOpen} onClose={onClose}>
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Update profile picture</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <input type="file" required onChange={updatePhoto} />
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="purple" onClick={onClose}>
+              Close
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 }
