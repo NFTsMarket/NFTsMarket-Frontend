07fc0374e88fc67a07d4340fec198a8a63c0445e
Jose Sandoval <joseasandovalp@gmail.com>
2022-01-18 13:16:47 +0100

    fix: add footer fix

--- components/layout/Footer.jsx
+++ components/layout/Footer.jsx
@@ -3,10 +3,12 @@ import { Box, Text } from "@chakra-ui/react";
 const Footer = () => (
   <Box
     as="footer"
+    position="absolute"
+    bottom={0}
     role="contentinfo"
     mx="auto"
     maxW="7xl"
-    py="12"
+    py={{ base: "4", md: "12" }}
     px={{ base: "4", md: "8" }}
   >
     <Text fontSize="sm" alignSelf={{ base: "center", sm: "start" }}>
